85e2f458eebbffa6e17734d7ef4ed93df8a754e6
Paul Boyer <pboyer2@wisc.edu>
2021-04-23 13:38:28 -0500

    added new file to repo

---
+++ Set-WACDelegatedCredentials.ps1
@@ -0,0 +1,7 @@
+<#
+    Script to enable SSO from WAC to endpoints. 
+    Paul B - 7-20-2020
+
+    See for more information: https://charbelnemnom.com/2019/07/how-to-enable-single-sign-on-sso-for-windows-admin-center/
+
+#>
