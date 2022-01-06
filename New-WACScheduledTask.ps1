e2a0169aafbdc8bdc4312a618d7973f3d98321ab
Paul Boyer <pboyer2@wisc.edu>
2022-01-06 09:07:36 -0600

    Changed New-WACSchdeduledTask to not be a function, just a script that takes in params.
    Signed-off-by: Paul Boyer <pboyer2@wisc.edu>

--- New-WACScheduledTask.ps1
+++ New-WACScheduledTask.ps1
@@ -1,117 +1,115 @@
-    function New-WACScheduledTask{
-    <#
-    .SYNOPSIS
-    This script creates Scheduled Tasks to run functions on a schedule to automate Windows Admin Center tasks.
-    
-    .DESCRIPTION
-    The script can be used to create a scheduled task to run a function on a schedule. You can use the function to setup a scheduled execution of either the connections management or extensions management script included in this repo.
-    
-    .PARAMETER ConfigFile
-    Path to the JSON configuration file.
-    
-    .PARAMETER Extensions
-    Switch parameter directing the script to register a scheduled task to run the extensions management script.
-    
-    .PARAMETER Connections
-    Switch parameter directing the script to register a scheduled task to run the connections management script.
-    
-    .PARAMETER ExtensionsFile
-    An override parameter to specify the path to the extensions management script. This supersedes the default path to the script configured in the JSON file.
-    
-    .PARAMETER ConnectionsFile
-    An override parameter to specify the path to the connections management script. This supersedes the default path to the script configured in the JSON file.
-    
-    .EXAMPLE
-    New-WACScheduledTask -ConfigFile "C:\WAC\WACScheduledTask.json" -Extensions
+<#
+.SYNOPSIS
+This script creates Scheduled Tasks to run functions on a schedule to automate Windows Admin Center tasks.
 
-    In this example the script will create a scheduled task to run the extensions management script.
+.DESCRIPTION
+The script can be used to create a scheduled task to run a function on a schedule. You can use the function to setup a scheduled execution of either the connections management or extensions management script included in this repo.
 
-    .EXAMPLE
-    New-WACScheduledTask -ConfigFile "C:\WAC\WACScheduledTask.json" -Connections -ConnectionsFile "C:\WAC\Connections.csv"
+.PARAMETER ConfigFile
+Path to the JSON configuration file.
 
-    In this example the script will create a scheduled task to run the connections management script, but will use the specified path to the connections management script rather than the one in the JSON file.
+.PARAMETER Extensions
+Switch parameter directing the script to register a scheduled task to run the extensions management script.
 
-    .LINK
-    http://woshub.com/group-managed-service-accounts-in-windows-server-2012/
-    
-    .NOTES
-        Author: Paul Boyer
-        Date: 01-06-2022
-    #>
-    param (
-        [Parameter(Mandatory=$true)]
-        [ValidateScript({
-            # Check that a path to a JSON file was passed
-            if([IO.Path]::getExtension($_) -eq ".json"){
-                $true
-            }else{
-                $false
-            }
-        })]
-        [String]
-        $ConfigFile,
-        [Parameter()]
-        [Switch]
-        $Extensions,
-        [Parameter()]
-        [Switch]
-        $Connections,
-        [Parameter()]
-        [string]
-        $ExtensionsFile,
-        [Parameter()]
-        [string]
-        $ConnectionsFile
-    )
-    
-    # Import the settings from the JSON config file
-    [Object]$CONFIG = Get-Content -Raw -Path $ConfigFile | ConvertFrom-Json
+.PARAMETER Connections
+Switch parameter directing the script to register a scheduled task to run the connections management script.
 
-    # Configure settings for the scheduled task. Applies to both Extensions and Connections tasks
-    $Settings = New-ScheduledTaskSettingsSet -ExecutionTimeLimit (New-TimeSpan -Hours 1)
+.PARAMETER ExtensionsFile
+An override parameter to specify the path to the extensions management script. This supersedes the default path to the script configured in the JSON file.
 
-    # Create a new scheduled task for Extension Management if $Extensions is true
-    if($Extensions){
-        [String] $ExtensionsAction
-        # Check if a value for the ExtensionsFile parameter was passed. Then assign the appropriate action to $ExtensionsAction
-        if ($ExtensionsFile -ne "") {
-            $ExtensionsAction = $ExtensionsFile
+.PARAMETER ConnectionsFile
+An override parameter to specify the path to the connections management script. This supersedes the default path to the script configured in the JSON file.
+
+.EXAMPLE
+New-WACScheduledTask -ConfigFile "C:\WAC\WACScheduledTask.json" -Extensions
+
+In this example the script will create a scheduled task to run the extensions management script.
+
+.EXAMPLE
+New-WACScheduledTask -ConfigFile "C:\WAC\WACScheduledTask.json" -Connections -ConnectionsFile "C:\WAC\Connections.csv"
+
+In this example the script will create a scheduled task to run the connections management script, but will use the specified path to the connections management script rather than the one in the JSON file.
+
+.LINK
+http://woshub.com/group-managed-service-accounts-in-windows-server-2012/
+
+.NOTES
+    Author: Paul Boyer
+    Date: 01-06-2022
+#>
+param (
+    [Parameter(Mandatory=$true)]
+    [ValidateScript({
+        # Check that a path to a JSON file was passed
+        if([IO.Path]::getExtension($_) -eq ".json"){
+            $true
         }else{
-            $ExtensionsAction = $CONFIG.ScheduledTask.Action.ExtensionPath
+            $false
         }
+    })]
+    [String]
+    $ConfigFile,
+    [Parameter()]
+    [Switch]
+    $Extensions,
+    [Parameter()]
+    [Switch]
+    $Connections,
+    [Parameter()]
+    [string]
+    $ExtensionsFile,
+    [Parameter()]
+    [string]
+    $ConnectionsFile
+)
 
-        # Create the action for the Extension Management scheduled task
-        $Action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-File `"$($ExtensionsAction)`" -ExecutionPolicy Bypass -NoLogo -NonInteractive -NoProfile -WindowStyle Hidden"
+# Import the settings from the JSON config file
+[Object]$CONFIG = Get-Content -Raw -Path $ConfigFile | ConvertFrom-Json
 
-        # Create the trigger for the Extension Management scheduled task
-        $Trigger = New-ScheduledTaskTrigger -Daily -At $($CONFIG.ScheduledTask.Trigger.ExtensionTime)
+# Configure settings for the scheduled task. Applies to both Extensions and Connections tasks
+$Settings = New-ScheduledTaskSettingsSet -ExecutionTimeLimit (New-TimeSpan -Hours 1)
 
-        # Create the principal for the Extension Management scheduled task
-        $Principal = New-ScheduledTaskPrincipal -UserId $($CONFIG.ScheduledTask.Principal.UserId) -LogonType Password -RunLevel Highest
-
-        # Create the scheduled task for Extension Management
-        Register-ScheduledTask -TaskPath "\WindowsAdminCenter" -TaskName "Windows Admin Center - Extension Management" -Action $Action -Trigger $Trigger -Principal $Principal -Settings $Settings -Description "Scheduled Task that runs every day at $($CONFIG.ScheduledTask.Trigger.ExtensionTime) to update and install WAC extensions." -Force
-    }elseif ($Connections) {
-        [String] $ConnectionsAction
-        # Check if a value for the ConnectionsFile parameter was passed. Then assign the appropriate action to $ConnectionsAction
-        if ($ConnectionsFile -ne "") {
-            $ConnectionsAction = $ConnectionsFile
-        }else{
-            $ConnectionsAction = $CONFIG.ScheduledTask.Action.ConnectionPath
-        }
-
-        # Create the action for the Connection Management scheduled task
-        $Action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-File `"$($ConnectionsAction)`" -ExecutionPolicy Bypass -NoLogo -NonInteractive -NoProfile -WindowStyle Hidden"
-
-        # Create the trigger for the Connection Management scheduled task
-        $Trigger = New-ScheduledTaskTrigger -Daily -At $($CONFIG.ScheduledTask.Trigger.ExtensionTime)
-
-        # Create the principal for the Connection Management scheduled task
-        $Principal = New-ScheduledTaskPrincipal -UserId $($CONFIG.ScheduledTask.Principal.UserId) -LogonType Password 
-
-        # Create the scheduled task for Connection Management
-        Register-ScheduledTask -TaskPath "\WindowsAdminCenter" -TaskName "Windows Admin Center - Connections Management" -Action $Action -Trigger $Trigger -Principal $Principal -Settings $Settings -Description "Scheduled Task that runs every day at $($CONFIG.ScheduledTask.Trigger.ConnectionTime) to update and import computer and server connections into WAC." -Force 
+# Create a new scheduled task for Extension Management if $Extensions is true
+if($Extensions){
+    [String] $ExtensionsAction
+    # Check if a value for the ExtensionsFile parameter was passed. Then assign the appropriate action to $ExtensionsAction
+    if ($ExtensionsFile -ne "") {
+        $ExtensionsAction = $ExtensionsFile
     }else{
-        Write-Error "No action specified. Please specify either -Extensions or -Connections."
+        $ExtensionsAction = $CONFIG.ScheduledTask.Action.ExtensionPath
     }
+
+    # Create the action for the Extension Management scheduled task
+    $Action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-File `"$($ExtensionsAction)`" -ExecutionPolicy Bypass -NoLogo -NonInteractive -NoProfile -WindowStyle Hidden"
+
+    # Create the trigger for the Extension Management scheduled task
+    $Trigger = New-ScheduledTaskTrigger -Daily -At $($CONFIG.ScheduledTask.Trigger.ExtensionTime)
+
+    # Create the principal for the Extension Management scheduled task
+    $Principal = New-ScheduledTaskPrincipal -UserId $($CONFIG.ScheduledTask.Principal.UserId) -LogonType Password -RunLevel Highest
+
+    # Create the scheduled task for Extension Management
+    Register-ScheduledTask -TaskPath "\WindowsAdminCenter" -TaskName "Windows Admin Center - Extension Management" -Action $Action -Trigger $Trigger -Principal $Principal -Settings $Settings -Description "Scheduled Task that runs every day at $($CONFIG.ScheduledTask.Trigger.ExtensionTime) to update and install WAC extensions." -Force
+}elseif ($Connections) {
+    [String] $ConnectionsAction
+    # Check if a value for the ConnectionsFile parameter was passed. Then assign the appropriate action to $ConnectionsAction
+    if ($ConnectionsFile -ne "") {
+        $ConnectionsAction = $ConnectionsFile
+    }else{
+        $ConnectionsAction = $CONFIG.ScheduledTask.Action.ConnectionPath
     }
+
+    # Create the action for the Connection Management scheduled task
+    $Action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-File `"$($ConnectionsAction)`" -ExecutionPolicy Bypass -NoLogo -NonInteractive -NoProfile -WindowStyle Hidden"
+
+    # Create the trigger for the Connection Management scheduled task
+    $Trigger = New-ScheduledTaskTrigger -Daily -At $($CONFIG.ScheduledTask.Trigger.ExtensionTime)
+
+    # Create the principal for the Connection Management scheduled task
+    $Principal = New-ScheduledTaskPrincipal -UserId $($CONFIG.ScheduledTask.Principal.UserId) -LogonType Password 
+
+    # Create the scheduled task for Connection Management
+    Register-ScheduledTask -TaskPath "\WindowsAdminCenter" -TaskName "Windows Admin Center - Connections Management" -Action $Action -Trigger $Trigger -Principal $Principal -Settings $Settings -Description "Scheduled Task that runs every day at $($CONFIG.ScheduledTask.Trigger.ConnectionTime) to update and import computer and server connections into WAC." -Force 
+}else{
+    Write-Error "No action specified. Please specify either -Extensions or -Connections."
+}
